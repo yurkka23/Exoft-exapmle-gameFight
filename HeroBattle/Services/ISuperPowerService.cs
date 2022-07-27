db4b805c4355843eaf3bf2b2cdb30f6516b999a4
Yevhen Volynets <yvolynets@exoft.net>
2022-07-27 15:00:36 +0300

    Added SuperPowerServices

---
+++ HeroBattle/Services/ISuperPowerService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using HeroBattle.Models;
+
+namespace HeroBattle.Services
+{
+    public interface ISuperPowerService
+    {
+        void AddSuperPower(BaseHero hero, int power);
+    }
+}
