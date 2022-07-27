000126765273a14c9123186c82dca6c961dcada4
Yevhen Volynets <yvolynets@exoft.net>
2022-07-27 14:38:20 +0300

    Added heros

---
+++ HeroBattle/Models/Heros/Tor.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace HeroBattle.Models.Heros
+{
+    public class Tor : BaseHero
+    {
+        public override int Armor { get; set; } = 4;
+
+        public override int Damage { get; set; } = 16;
+    }
+}
